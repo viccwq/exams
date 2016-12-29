c39ab5189778dfd8bcfcd300cb5fda658d1a983b
viccwq <vic_cwq@163.com>
2016-12-29 23:25:55 +0800

    add code

--- jianzhi_offer.cpp
+++ jianzhi_offer.cpp
@@ -447,6 +447,69 @@ public:
 };
 
 
+//输入两棵二叉树A，B，判断B是不是A的子结构。（ps：我们约定空树不是任意一个树的子结构）
+/*
+struct TreeNode {
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+	TreeNode(int x) :
+			val(x), left(NULL), right(NULL) {
+	}
+};*/
+class Solution {
+public:
+    bool HasSubtree(TreeNode* pRoot1, TreeNode* pRoot2)
+    {
+        bool result = false;
+
+        if (pRoot1 && pRoot2)
+        {
+            if (pRoot1->val == pRoot2->val)
+            {
+                result = DoesTree1HasTree2(pRoot1, pRoot2);
+            }
+            if (!result)
+            {
+                result = HasSubtree(pRoot1->left, pRoot2);
+            }
+            if (!result)
+            {
+                result = HasSubtree(pRoot1->right, pRoot2);
+            }
+        }
+
+        return result;
+    }
+
+    bool DoesTree1HasTree2(TreeNode* root1, TreeNode* root2)
+    {
+        if (!root2)
+        {
+            return true;
+        }
+
+        if (!root1)
+        {
+            return false;
+        }
+
+        if (root1->val != root2->val)
+        {
+            return false;
+        }
+        return DoesTree1HasTree2(root1->left, root2->left) \
+            && DoesTree1HasTree2(root1->right, root2->right);
+        
+    }
+
+
+};
+
+
+
+
+
 //删除链表中重复的结点
 //在一个排序的链表中，存在重复的结点，请删除该链表中重复的结点，重复的结点不保留，返回链表头指针。 例如，链表1->2->3->3->4->4->5 处理后为 1->2->5
 
