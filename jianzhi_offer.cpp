6eabbb691bd4765f3e6d5b1c56a237c26efa297b
viccwq <vic_cwq@163.com>
2017-01-20 11:45:27 +0800

    add

--- jianzhi_offer.cpp
+++ jianzhi_offer.cpp
@@ -1885,6 +1885,54 @@ public:
 
 
 
+//在一个长度为n的数组里的所有数字都在0到n-1的范围内。 
+//数组中某些数字是重复的，但不知道有几个数字是重复的。
+//也不知道每个数字重复几次。请找出数组中任意一个重复的数字。 
+//例如，如果输入长度为7的数组{2,3,1,0,2,5,3}，那么对应
+//的输出是重复的数字2或者3。
+
+class Solution {
+public:
+	// Parameters:
+	//        numbers:     an array of integers
+	//        length:      the length of array numbers
+	//        duplication: (Output) the duplicated number in the array number
+	// Return value:       true if the input is valid, and there are some duplications in the array number
+	//                     otherwise false
+	bool duplicate(int numbers[], int length, int* duplication) {
+		if (length <= 1)
+			return false;
+		
+		bool ret = true;
+		int idx = -1;
+
+		int *cnt = new int[length];
+		memset(cnt, 0x0, sizeof(int) * length);
+	
+		for (int i = 0; i < length; i++)
+		{			
+			if (numbers[i] >= 0 && numbers[i] < length)
+			{
+				if ((++cnt[numbers[i]]) >= 2)
+					idx = i;
+			}
+			else
+			{
+				ret = false;
+				break;
+			}
+		}
+		if (ret && (idx >= 0))
+			*duplication = numbers[idx];
+		else
+			ret = false;
+
+		delete []cnt;
+		return ret;
+
+
+	}
+};
 
 
 
