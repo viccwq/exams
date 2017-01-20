f663a589afe7bb11bb1aaf084fb70840c28eae0e
viccwq <vic_cwq@163.com>
2017-01-20 10:55:21 +0800

    add

--- jianzhi_offer.cpp
+++ jianzhi_offer.cpp
@@ -1837,7 +1837,51 @@ public:
 };
 
 
+// 将一个字符串转换成一个整数，要求不能使用字符串转换整数的库函数。
+// 数值为0或者字符串不是一个合法的数值则返回0 
+// 输入描述:
+// 输入一个字符串,包括数字字母符号,可以为空
+// 
+// 
+// 输出描述:
+// 如果是合法的数值表达则返回该数字，否则返回0
+// 
+// 输入例子:
+// +2147483647
+// 	1a33
+// 
+// 输出例子:
+// 2147483647
+// 	0
 
+class Solution {
+public:
+	int StrToInt(string str) {
+		int len = str.length();
+		if (0 == len || str.empty())
+			return 0;
+
+		int ret = 0;
+		int flag = 1;
+		int i;
+		for (i = len - 1; i >= 0; i--)
+		{
+			if (str[i] >= '0' && str[i] <= '9')
+				ret += ((str[i] - '0') * flag);
+			else if (str[i] == ' ' || str[i] == '+' || str[i] == '-')
+				break;
+			else
+				return 0;
+			flag *= 10;
+		}
+
+		if(str[i] == '-')
+			return ret * (-1);
+		else
+			return ret;
+
+	}
+};
 
 
 
