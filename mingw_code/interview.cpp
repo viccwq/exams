544788a08fee318875fa8919f92443bcdb0736a5
viccwq <vic_cwq@163.com>
2017-02-17 21:01:05 +0800

    add interview.cpp

---
+++ mingw_code/interview.cpp
@@ -0,0 +1,38 @@
+/*************************************************************************
+ * Copyright(c) 2016-2020 VX-Lab. All rights reserved.
+ * File Name:    interview.c
+ * Author:       Vic Chen
+ * Mail:         vic_cwq@163.com 
+ * Version:      v0.00 
+ * Created Time: 2017/02/17
+ * Description:  
+ * 
+ * Others:       
+ * 
+ * History:
+ 1.Version:
+   Author:
+   Modified Time:
+   Modification:
+ ************************************************************************/
+
+
+#include "interview.h"
+
+#include <stdio.h>
+
+//链表排序
+
+
+//洗牌
+
+
+//string的构造 析构 赋值
+
+
+//static and const
+
+
+//什么是过度拟合
+
+//单线程的实现
