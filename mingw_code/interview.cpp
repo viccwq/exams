d39c2fa9ede0dbcc8b7ee392120850627e2790f1
viccwq <vic_cwq@163.com>
2017-05-27 17:09:27 +0800

    add code to interview.cpp

--- mingw_code/interview.cpp
+++ mingw_code/interview.cpp
@@ -22,17 +22,13 @@
 #include <stdlib.h>
 #include <iostream>
 using namespace std;
+
+
+
+
 //c和C++程序员面试秘籍
-//chapter6 28th
 
-
-
-//链表排序
-
-
-//洗牌
-
-
+//chapter6 26th
 //string的构造 析构 赋值说明几点
 //http://www.cnblogs.com/kaituorensheng/p/3245522.html
 //http://blog.csdn.net/zhuimengzh/article/details/6708882
@@ -104,7 +100,7 @@ MyString::~MyString()
     delete [] m_data;
 }
 
-int main()
+int test_626()
 {  
     MyString a;  
     MyString b("abc");  
@@ -117,6 +113,102 @@ int main()
     return 0;
 } 
 
+//chapter6 28th
+class B628
+{
+public:
+    B628()
+    {
+        cout<<"default constructor"<<endl;
+    }
+    B628(const B628 &b)
+    {
+        cout<<"copy constructor"<<endl;
+        data = b.data;
+    }
+    /*
+    */
+    B628(int i):data(i)
+    {
+        cout<<"constructed by parameter"<<data<<endl;
+    }
+    ~B628()
+    {
+        cout<<"destructed"<<endl;
+    }
+private:
+    int data;
+};
+
+B628 Play(B628 b)
+{
+    return b;
+}
+
+int test_628()
+{
+    B628 t1 = Play(5);
+    B628 t2 = Play(t1);
+    return 0;
+}
+
+
+
+
+//chapter7 6th
+//编译时的多态--重载实现
+//运行时的多态--虚成员实现
+//
+class Person706
+{
+public:
+    virtual void print()
+    {
+        cout<<"I'm a Person"<<endl;
+    }
+};
+class Chinese706:public Person706
+{
+public:
+    virtual void print()
+    {
+        cout<<"I'm a Chinese Person"<<endl;
+    }
+};
+class American706:public Person706
+{
+public:
+    virtual void print()
+    {
+        cout<<"I'm a American Person"<<endl;
+    }
+};
+
+void printPerson(Person706 &per)
+{
+    per.print();
+}
+
+int test_706()
+{
+    Person706 p;
+    Chinese706 c;
+    American706 a;
+    printPerson(p);
+    printPerson(c);
+    printPerson(a);
+    return 0;
+}
+
+
+
+
+
+//链表排序
+//
+
+//洗牌
+//
 //static and const
 
 
@@ -124,3 +216,14 @@ int main()
 
 //单线程的实现
 //利用struct实现class的功能
+//数独
+//光流算法(目标追踪)
+//方向定位(司机看车辆)
+//
+//
+int main()
+{
+//    test_626();
+//    test_628();
+    test_706();
+}
